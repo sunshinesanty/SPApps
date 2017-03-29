be54ae95eeb9f55676f7b3928db85739fd175978
sunshinesanty <iyer.santoshkumar@gmail.com>
2017-03-29 21:33:20 +1100

    changes

---
+++ chatalong/src/components/chat.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {IChatState, IChatProps} from '../interfaces/ChatInterfaces';
+
+class Chat extends React.Component<IChatProps, IChatState> {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+export default Chat;
---
+++ chatalong/src/interfaces/ChatInterfaces.ts
@@ -0,0 +1,43 @@
+export interface IChatProps {
+    chatsperpage: number;
+    lastmodifieddate: number;
+}
+export interface IUser{
+    id: number;
+    name: text;
+}
+export interface ILikesCollection {
+    likes: ILike[];
+    likecount: number;
+}
+export interface ICommentsCollection {
+    comments: IComment[];
+    commentcount: number;
+}
+export interface ILike {
+    id: number;
+    conversationID: number,
+    userid: number;
+    created: string;
+    modifed: string;
+}
+export interface IComment {
+    commentText: string;
+    id: number;
+    conversationID: number,
+    userid: number;
+    created: string;
+    modifed: string;
+}
+export interface IConversation{
+    id: number;
+    text: string;
+    comments: ICommentsCollection;
+    likes: ILikesCollection;
+    userid: number;
+    created: string;
+    modifed: string;
+}
+export interface IChatState {
+    conversations: IConversation[];
+}
