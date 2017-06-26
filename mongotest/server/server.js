87e771401216ddbc0864bb6c7da4668a4fc4e219
Santoshkumar Krishnamurthy <santoshkumar.krishna@oakton.com.au>
2017-06-26 14:00:45 +1000

    changes made to add fronmtened

--- mongotest/server/server.js
+++ mongotest/server/server.js
@@ -1,12 +1,17 @@
 "use strict";
 exports.__esModule = true;
 var express = require("express");
+var mongodb_1 = require("mongodb");
 var path = require("path");
 var server = express();
-server.use(express.static('dist'));
-server.get('/', function (req, res) {
-    res.sendFile(path.resolve('index.html'));
-});
-server.listen(9005, function () {
-    console.log('Example app listening on port 3000!');
+mongodb_1.MongoClient.connect('mongodb://admin:Melbourne1@ds147480.mlab.com:47480/chat', function (err, database) {
+    if (err)
+        console.log(err);
+    server.use(express.static('dist'));
+    server.get('/', function (req, res) {
+        res.sendFile(path.resolve('index.html'));
+    });
+    server.listen(9005, function () {
+        console.log('Example app listening on port 3000!');
+    });
 });
--- mongotest/src/server/server.ts
+++ mongotest/src/server/server.ts
@@ -1,14 +1,16 @@
 import * as express from 'express';
-import mongodb from 'mongodb';
+import mongoose from 'mongoose';
 import * as path from 'path';
 var server = express();
+var db = mongoose.connect('mongodb://admin:Melbourne1@ds147480.mlab.com:47480/chat');
+db.    
+    server.use(express.static('dist'));
 
-server.use(express.static('dist'));
+    server.get('/', function (req, res) {
+        res.sendFile(path.resolve('index.html'));
+    })
 
-server.get('/', function (req, res) {
-    res.sendFile(path.resolve('index.html'));
-})
+    server.listen(9005, function () {
+        console.log('Example app listening on port 3000!')
+    })
 
-server.listen(9005, function () {
-    console.log('Example app listening on port 3000!')
-})
